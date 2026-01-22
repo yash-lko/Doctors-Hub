fac79f969010c2f706edae7cb20fd84d7e845746
yashwant <yashwantbit@gmail.com>
2026-01-22 12:21:00 +0530

    Doctors testing

--- src/context/DoctorContext.js
+++ src/context/DoctorContext.js
@@ -8,7 +8,7 @@ export const DoctorProvider = ({ children }) => {
   const [doctors, setDoctors] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  console.log(doctors, "Doctors list")
 
   useEffect(() => {
     const fetchDoctors = () => {
